1a3b996e13e0e236df7ccdbbdcde2acc5bd8b501
Kenan <kenan.karic@outlook.com>
2021-07-11 12:28:54 +0200

    Added splash screen

--- app/src/main/java/io/aethibo/rollback/features/MainActivity.kt
+++ app/src/main/java/io/aethibo/rollback/features/MainActivity.kt
@@ -7,6 +7,7 @@ import io.aethibo.rollback.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Rollback)
         setContentView(R.layout.activity_main)
     }
 }
